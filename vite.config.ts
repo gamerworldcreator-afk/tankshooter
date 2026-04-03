0d34c3a7e4f6db323fdf552ac14e4ead49603a49
Karthikeyan Kanagaraj <gamerworld.creator@gmail.com>
2026-04-03 23:56:54 +0530

    Adding Github Actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/tankshooter/' : '/',
   build: {
     target: 'es2022',
     sourcemap: false,
@@ -14,4 +15,4 @@ export default defineConfig({
       }
     }
   }
-});
+}));
